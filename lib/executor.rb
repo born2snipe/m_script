1024e81b1eb0a3b39de1780a639bea3b7ee61b07
born2snipe <born2snipe@gmail.com>
2009-11-28 10:16:34 -0600

    pulled the config as a field changed to no longer depend on the ampersand pair, instead using the result from the system command to determine if the build should continue running

--- lib/executor.rb
+++ lib/executor.rb
@@ -7,45 +7,46 @@ module MScript
     def initialize()
       @file_util = MScript::FileUtil.new
       @arg_parser = MScript::ArgParser.new
+      @config = MScript::Config.new(@file_util.locate_project_directory(File.expand_path('.')))
     end
     
     def execute(args)
-      config = MScript::Config.new(@file_util.locate_project_directory(File.expand_path('.')))
       builds = @arg_parser.parse(args)
       
       total_builds = builds['projects'].length
       
       commands = []
       builds['projects'].each do |project|
-        directory_alias = project['project']
-        phase_aliases = project['phases']
+        directory = @config.to_directory(project['project'])
+        phases = @config.to_phases(project['phases'])
         
-        commands << "mvn #{config.to_phases(phase_aliases).join(' ')} -f #{File.join(config.to_directory(directory_alias), 'pom.xml')} #{builds['arguments']}"
+        commands << "mvn #{phases.join(' ')} -f #{File.join(directory, 'pom.xml')} #{builds['arguments']}"
       end
       
       # was done this way because Control+C does not kill all builds?? WTF?!
-      command = commands.join(' && ')
+      result = true
+      i = 0
+      while result && i < commands.length
+        command = commands[i]      
+        puts "------------------------------"
+        puts "M Script Running....#{i+1}/#{total_builds} build(s)" 
+        puts "------------------------------"
+        puts "#{command}"
+        puts "------------------------------\n"
       
-      puts "------------------------------"
-      puts "M Script Running....#{total_builds} build(s)" 
-      puts "------------------------------"
-      puts "#{command}"
-      puts "------------------------------\n"
-      
-      system command
-      
+        result = system command
+        i += 1
+      end
     end
     
     def show_help()
-      config = MScript::Config.new(@file_util.locate_project_directory(File.expand_path('.')))
-      
       puts "\n"
-      puts "Project Directory: #{config.project_directory}\n\n"
+      puts "Project Directory: #{@config.project_directory}\n\n"
       puts "Directory to Alias(es):\n---------------------------\n"
       
       longest_name = ""
       names = []
-      config.directory_aliases.each do |key, value|
+      @config.directory_aliases.each do |key, value|
         if (key.length > longest_name.length)
           longest_name = key
         end
@@ -53,7 +54,7 @@ module MScript
       end
       
       names.sort { |a, b| a <=> b}.each do |directory|
-        aliases = config.directory_aliases[directory]
+        aliases = @config.directory_aliases[directory]
         number_of_dots = longest_name.length + 2 - directory.length
         count = 0
         dots = ""
@@ -65,7 +66,7 @@ module MScript
       end
       
       puts "\nAvailable Phase(s):\n---------------------------\n"
-      config.phases.each do |key, value|
+      @config.phases.each do |key, value|
         puts "#{key}....#{value}"
       end
       puts "\n"
